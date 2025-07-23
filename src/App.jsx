8512f49d4c14ded4420bfcb440777eea93e375f2
elliotgaramendi <elliotgaramendi@gmail.com>
2025-07-22 21:40:25 -0500

    feat: add Home, About, and Movies components for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router"
 import Header from "./components/Header"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Movies from "./pages/Movies"
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/movies" element={<h1>Movies</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   )
