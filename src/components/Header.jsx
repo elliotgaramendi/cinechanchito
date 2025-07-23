2492a5aa881a5f228a4d97c94dc3a94ff3db0d1e
elliotgaramendi <elliotgaramendi@gmail.com>
2025-07-22 21:36:55 -0500

    fix: update Header component to use container class and change button style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
+        <div className="container">
           <Link className="navbar-brand" to="/">CineChanchi</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -34,7 +34,7 @@ const Header = () => {
             </ul>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <button className="btn btn-outline-info" type="submit">Search</button>
             </form>
           </div>
         </div>
