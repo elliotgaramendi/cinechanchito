e6d1ecf1286bfbde4546a3304a0883eab62353d2
elliotgaramendi <elliotgaramendi@gmail.com>
2025-07-22 20:56:46 -0500

    feat: implement routing with BrowserRouter and add About page

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
