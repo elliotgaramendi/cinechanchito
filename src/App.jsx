34a07f310b51ab8627591213f7b7f92dce570931
elliotgaramendi <elliotgaramendi@gmail.com>
2025-07-22 21:14:30 -0500

    feat: refactor App component to include Header and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router"
+import Header from "./components/Header"
+
 function App() {
   return (
     <>
-      <h1>Cinechanchito</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/movies" element={<h1>Movies</h1>} />
+      </Routes>
     </>
   )
 }
