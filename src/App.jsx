6c7ea3b6510520dc0504c2049716fe7fa1b927b1
elliotgaramendi <elliotgaramendi@gmail.com>
2025-07-24 22:00:51 -0500

    feat: add Footer component and integrate it into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router"
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import About from "./pages/About"
 import Home from "./pages/Home"
@@ -13,6 +14,7 @@ function App() {
         <Route path="/movies" element={<Movies />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </>
   )
 }
